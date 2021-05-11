7dca30f9294309cf30d1e4beafa72ec91c906b8f
pcjs156 <qkrdbqls1001@naver.com>
2021-05-12 01:45:05 +0900

    Header file 구현: Snake.h, Manager.h, Objects.h 수연님이 작성한 설계 문서를 바탕으로 대략적인 인터페이스를 구현함 단, 현재 구현이 완료되지 않아 대략적인 흐름만 알 수 있도록 되어 있기 때문에 반드시 수정될 예정이고,  과제 구현 사항의 각 단계마다 명세가 조금씩 달라질 수 있으므로 유의해야 함. 자세한 내용은 주석 참고.

---
+++ Snake.h
@@ -0,0 +1,192 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include "Manager.h"
+using namespace std;
+
+/* Snake의 상태를 저장/갱신하기 위한 클래스 */
+class Snake
+{
+private:
+    const static int MIN_LENGTH = 3; // 뱀의 최소 몸길이
+    int length;                      // 뱀의 몸 길이
+    int growthCnt;                   // Growth 획득 횟수
+    int poisonCnt;                   // Posion 획득 횟수
+    int gateCnt;                     // Gate 이용 횟수
+    Direction lastDirection;         // Snake의 마지막 이동 방향
+    vector<Body> bodies;             // Snake의 머리와 몸통들을 요소로 하는 벡터
+
+public:
+    // Constructor
+    Snake(int initX, int initY)
+    {
+        // 몸 길이 초기화
+        length = 3;
+        // 아이템 획득/게이트 통과 횟수 초기화
+        growthCnt = 0;
+        poisonCnt = 0;
+        gateCnt = 0;
+
+        // 왼쪽 방향을 기본 방향으로 함
+        Direction lastDirection = Direction::getDirectionBySymbol('L');
+
+        // 맨 처음에는 ">~~" 이렇게 왼쪽을 보고 움직이도록 설정
+        bodies.push_back(Body(initX, initY, initX - 1, initY));
+        bodies.push_back(Body(initX + 1, initY, initX, initY));
+        bodies.push_back(Body(initX + 2, initY, initX + 1, initY));
+    }
+
+    // Getter ============================================
+    int getLength() { return length; };
+    int getGrowthCnt() { return growthCnt; };
+    int getPoisonCnt() { return poisonCnt; };
+    int getGateCnt() { return gateCnt; };
+    /* body의 맨 앞 레퍼런스를 반환함
+       Precondition: bodies가 비어 있으면 안됨 */
+    Body getHead();
+    /* head의 좌표를 Pos로 반환함 */
+    Pos getHeadPos();
+
+    // Setter =============================================
+    void setLength(int length) { this->length = length; };
+    void setGrowhCnt(int cnt) { this->growthCnt = cnt; };
+    void setPoisonCnt(int cnt) { this->poisonCnt = cnt; };
+    void setGateCnt(int cnt) { this->gateCnt; };
+
+    // 몸 길이 관련 ========================================
+    /* Snake의 길이를 1만큼 줄이는 함수
+     길이를 더 줄일 수 없다면(줄이면 뱀이 죽는다면) 길이를 갱신하지 않고 false를 반환
+     길이를 더 줄일 수 있다면 길이와 Body 정보를 갱신하고 true를 반환 */
+    bool shorten();
+    /* Snake의 길이를 1만큼 늘이고 Body 정보를 갱신하는 함수
+     몸 길이의 상한선이 없으므로 항상 잘 작동하지만, 일관성을 위해 항상 true를 반환 */
+    bool lengthen(Direction newDirection);
+
+    // 아이템 관련 =========================================
+    /* 함수 내부에서 item.affect(*this)를 수행해 snake의 상태를 갱신하고, 게임 종료 여부를 판단함 */
+    bool useItem(Manager &m, Item &item);
+    /* 게이트를 사용해 Body의 위치와 이동 경로를 수정함. 일관성을 위해 항상 true를 반환하도록 설정 */
+    bool useGate(Manager &m, Direction direction);
+
+    // 이동 관련 ============================================
+    /* newDirection 방향으로 Snake를 이동시키고, lastDirection을 갱신함
+       만약 newDirection이 lastDirection과 반대 방향이라면, false를 반환해 게임을 종료할 수 있도록 하고
+       그렇지 않은 경우 true를 반환하여 메서드가 제대로 실행 되었음을 외부에 알림
+       단, 아이템이나 벽 충돌, 몸통 충돌 등으로 인한 게임 종료는 함수 외부에서 판단함 */
+    bool moveTo(Direction newDirection);
+
+    // MapInfo와의 상호작용 ==================================
+    /* 게임 매니저와 상호작용하여 현재 벽에 부딪힌 상태인지(머리가 벽과 겹쳐 있는지) 확인하는 메서드 
+       벽에 부딪힌 상태라면 true, 그렇지 않다면 false를 반환함 */
+    bool isBumpedToWall(Manager &m);
+
+    // 상태 점검 =============================================
+    /* 머리가 자신의 몸통과 부딪혔는지 확인하는 메서드
+       Precondition: moveTo 메서드에 의해 이동이 완전히 끝난 후에 호출해야 함 */
+    bool isBumpedToBody();
+};
+
+/* 방향을 표시하기 위한 클래스. 좌표와 심볼로 표현할 수 있음. */
+class Direction
+{
+private:
+    const int x;
+    const int y;
+
+public:
+    Direction() : x(0), y(0) {}
+
+    Direction(int x, int y) : x(x), y(y) {}
+
+    // char형 매개 변수(이하 symbol)를 받아 Direction 객체를 만들어 반환함
+    static Direction getDirectionBySymbol(char symbol)
+    {
+        switch (symbol)
+        {
+        case 'L':
+            return Direction(-1, 0);
+        case 'R':
+            return Direction(1, 0);
+        case 'U':
+            return Direction(0, -1);
+        case 'D':
+            return Direction(0, 1);
+        }
+
+        cout << "WARNING: Invalid Direction Symbol '" << symbol << "'\n";
+        cout << "Direction(0, 0) is returned.\n";
+        return Direction(0, 0);
+    }
+
+    // 해당 객체의 방향을 symbol로 표현해 반환
+    char getSymbol()
+    {
+        // 왼쪽
+        if (x == -1 && y == 0)
+            return 'L';
+        // 오른쪽
+        else if (x == 1 && y == 0)
+            return 'R';
+        // 위쪽
+        else if (x == 0 && y == -1)
+            return 'U';
+        // 아래쪽
+        else if (x == 0 && y == 1)
+            return 'D';
+        // 알 수 없는 경우
+        else
+            return 'X';
+    }
+};
+
+/* 좌표를 직관적으로 표시하기 위한 클래스 */
+class Pos
+{
+public:
+    int x;
+    int y;
+
+public:
+    Pos() {}
+
+    Pos(int x, int y) : x(x), y(y) {}
+};
+
+/* Snake를 구성하는 몸통(+머리) */
+class Body
+{
+private:
+    // Body의 현재 위치
+    Pos currentPos;
+    /* Body가 앞으로 이동해야 할 좌표들의 모음
+       update를 수행할 때마다 현재 좌표를 맨 앞의 요소로 갱신하고,
+       해당 스케줄을 삭제한다. */
+    vector<Pos> scheduleQueue;
+
+public:
+    Body() {}
+
+    // 항상 최초 위치 바로 이후의 스케줄까지 입력받아야 한다.
+    Body(int initX, int initY, int nextX, int nextY)
+    {
+        currentPos = Pos(initX, initY);
+        scheduleQueue.push_back(Pos(nextX, nextY));
+    }
+
+    /* Body의 현재 위치를 다음 위치로 갱신하고, 수행된 스케줄을 삭제한다.
+       수행 결과에 따라 다음의 값을 반환한다.
+       true: 정상 수행
+       false: scheduleQueue가 비어 있는 경우 */
+    bool update()
+    {
+        if (scheduleQueue.size() == 0)
+            return false;
+
+        // 현재 위치 갱신
+        currentPos = scheduleQueue[0];
+        // 맨 앞의 스케줄 삭제
+        scheduleQueue.erase(scheduleQueue.begin());
+
+        return true;
+    }
+};
